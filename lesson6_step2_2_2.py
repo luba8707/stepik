da4f9c789214613a97adc271de9100371b5aeef9
Liubov <Luba8707@gmail.com>
2020-07-13 18:05:10 +0400

    exp nomber 2 new commit

---
+++ lesson6_step2_2_2.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+import time 
+import math
+
+def calc(x):
+   return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://SunInJuly.github.io/execute_script.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+
+    y = calc(x)
+    print(y)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    button = browser.find_element_by_css_selector("button.btn")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+    option1 = browser.find_element_by_id("robotsRule")
+    option1.click()
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+    
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(20)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
