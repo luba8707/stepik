acb8fcb6ad8f7c11833dc3ae278e223076589529
Liubov <Luba8707@gmail.com>
2020-07-13 18:08:12 +0400

    exp nomber3 new commit

---
+++ lesson6_step2_3_1.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import time 
+import math
+import os 
+
+def calc(x):
+   return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_css_selector('button.btn')
+    button.click()
+
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+    print(y)
+
+    input1 = browser.find_element_by_id('answer')
+    input1.send_keys(y)
+
+    button = browser.find_element_by_css_selector('button.btn')
+    button.click()
+  
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(20)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
